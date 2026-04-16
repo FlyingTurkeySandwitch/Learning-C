ea6011b96167a8ad5430e3fad099bf5e5de95168
FlyingTurkeySandwitch <dkim5@oxy.edu>
2026-04-15 19:35:54 -0700

    now takes user input r

--- Week1/calculate_area_circle.c
+++ Week1/calculate_area_circle.c
@@ -7,10 +7,12 @@ double calculate_area_circle(double r, double p){
     return p * pow(r,2);
 }
 
-
+    
+    
 
 int main(void){
-    double r = 3.0;
-
+    double r;
+    printf("enter radius:");
+    r = scanf("%f", &r);
     printf("area of circle with radius %f is %f", r, calculate_area_circle(r,PI));
 }
